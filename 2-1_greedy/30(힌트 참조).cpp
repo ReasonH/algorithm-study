5ba6dc4b10b175f0f36b4bdfc8c5acfda875c668
YooHyuk <azsxd34@naver.com>
2019-05-16 01:33:26 +0900

    Release comment block

--- 2-1_greedy/30(힌트 참조).cpp
+++ 2-1_greedy/30(힌트 참조).cpp
@@ -1,25 +1,25 @@
-//#include <iostream>
-//#include <string>
-//#include <algorithm>
-//using namespace std;
-//
-//int main()
-//{
-//	string input;
-//	cin >> input;
-//	if (input.find('0') == string::npos)
-//		cout << "-1";
-//	else {
-//		int sum = 0;
-//		for (int i = 0; i < input.size(); i++)
-//			sum += input[i];
-//		if (sum % 3 != 0)
-//			cout << "-1";
-//		else
-//		{
-//			sort(input.begin(), input.end(), greater<char>());
-//			cout << input;
-//		}
-//	}
-//	return 0;
-//}
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	string input;
+	cin >> input;
+	if (input.find('0') == string::npos)
+		cout << "-1";
+	else {
+		int sum = 0;
+		for (int i = 0; i < input.size(); i++)
+			sum += input[i];
+		if (sum % 3 != 0)
+			cout << "-1";
+		else
+		{
+			sort(input.begin(), input.end(), greater<char>());
+			cout << input;
+		}
+	}
+	return 0;
+}
